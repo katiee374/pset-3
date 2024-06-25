d6c65cfb4b228baadb7f01de09fab17df0968fbb
Katelynn Emerson <ke101@wellesley.edu>
2024-06-25 10:50:49 -0400

    added pt. 2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# pset-3
+# PSET 3: Inheritance & Polymorphism
+## Part 2: Restructuring the Adventure Game
+In this part of the assignment, you should restructure your adventure game to 
+follow an inheritance structure. You should create a Thing class and modify 
+Actor and Room to inherit from it. Make a copy of Thing.java and modify your 
+existing Actor and Room files.
---
+++ Thing.java
@@ -0,0 +1,26 @@
+
+/**
+ * Instructions:
+ * This class will be the parent class of Actor and Room. Include the name and 
+ * description as instance variables of Thing, as well as any other instance 
+ * variables that Room and Actor might share. Then, move any common methods 
+ * that are in both classes to this class, and write a constructor. Modify 
+ * the Room and Actor classes to extend Thing, and remove any redundant code.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Thing
+{
+    // instance variables from Room and Actor here
+
+    /**
+     * Constructor for objects of class Thing
+     */
+    public Thing()
+    {
+        //initialize those instance variables
+    }
+
+    //write methods that Room and Actor have in common here
+}
