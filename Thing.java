dcb8e8c99b23ee3e3175b554ce4139fc75e2d261
Katelynn Emerson <ke101@wellesley.edu>
2024-06-27 11:05:10 -0400

    updated Thing/adv game task

--- Thing.java
+++ Thing.java
@@ -1,18 +1,23 @@
 
 /**
  * Instructions:
- * This class will be the parent class of Actor and Room. Include the name and 
- * description as instance variables of Thing, as well as any other instance 
- * variables that Room and Actor might share. Then, move any common methods 
- * that are in both classes to this class, and write a constructor. Modify 
- * the Room and Actor classes to extend Thing, and remove any redundant code.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * This class will be the parent class of Actor, Room, and Treasure. Include 
+ * the name and description as instance variables of Thing, as well as any 
+ * other instance variables that Room, Actor, and Treasure might share. Then, 
+ * move any common methods that are in both classes to this class, and write a 
+ * constructor. Modify the Room, Actor, and Treasure classes to extend Thing, 
+ * and remove any redundant code.
+ * 
+ * Finally, write a description of the class and update the author and version
+ * tags.
+ * 
+ * Write a description of class Thing here.
+ * @author CS200 Instructors
+ * @version 6.27.2024
  */
 public class Thing
 {
-    // instance variables from Room and Actor here
+    // instance variables from Room, Actor, and Treasure here
 
     /**
      * Constructor for objects of class Thing
@@ -22,5 +27,5 @@ public class Thing
         //initialize those instance variables
     }
 
-    //write methods that Room and Actor have in common here
+    //write methods that Room, Treasure, and Actor have in common here
 }
