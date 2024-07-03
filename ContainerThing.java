385d009f3861a5ead76f2f744df641c22aa0de34
Katelynn Emerson <ke101@wellesley.edu>
2024-07-03 13:59:42 -0400

    updated game inheritance

--- ContainerThing.java
+++ ContainerThing.java
@@ -1,18 +1,19 @@
 /**
  * Instructions:
  * Write this class to represent a holder of things, like a chest or bowl.
- * It descends from ThingHolder.java, but should have isOpen and openable fields
- * representing whether the object is open or can be opened.
+ * It descends from ThingHolder.java, but should have isOpen and openable 
+ * fields representing whether the object is open or can be opened.
  * 
- * Finally, add a description of the class and update the author and version tags.
+ * Finally, add a description of the class and update the author and version 
+ * tags.
  * 
  * @author CS200 Instructors
  * @version 7.1.2024
  */
 public class ContainerThing extends ThingHolder {
     /*
-     * TODO: Add instance variables, including isOpen and openable, both of type 
-     * boolean.
+     * TODO: Add instance variables, including isOpen and openable, both of 
+     * type boolean.
      */
 
     /*
--- ThingHolder.java
+++ ThingHolder.java
@@ -2,8 +2,10 @@
  * Instructions:
  * Write the following class to represent a collection of Thing objects
  * (actors, rooms, or treasures). It should have an instance variable that 
- * is an ArrayList of type Thing, as well as a name and description. In the 
- * toString method, describe the list as well as all the Things within it.
+ * is an ArrayList of type Thing, as well as any other variables you see fit. 
+ * In the toString method, describe the list as well as all the Things within 
+ * it. In the constructor, call super() to set the name and description.
+ * ThingHolder can extend class Thing because it has a name and description.
  * 
  * Finally, add a description of the class and update the author and version 
  * tags.
@@ -11,7 +13,7 @@
  * @author CS200 Instructors
  * @version 7.1.2024
  */
-public class ThingHolder {
+public class ThingHolder extends Thing {
     /*
      * TODO: Add instance variables.
      */
@@ -20,7 +22,7 @@ public class ThingHolder {
      * TODO: Write constructor
      */
     
-     /*
+    /*
      * TODO: Write getter, setter, and toString methods.
      * Make sure you add getter/setter methods that can add/remove single 
      * objects as well as a full list.
